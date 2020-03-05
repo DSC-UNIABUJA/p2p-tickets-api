afa1d5fb7d021ff64e7c2469d3769cf3c3f079da
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-03-05 20:38:34 +0100

    Modified eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,13 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   env: {
+    browser: true,
     node: true,
+    es6: true,
   },
-  rules: {},
+  rules: {
+    'valid-jsdoc': 'off',
+  },
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
