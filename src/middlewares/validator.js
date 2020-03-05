f02249012c86ca9629776bc359deba09e6a3d059
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-03-05 20:39:15 +0100

    Wrote validator middleware

---
+++ src/middlewares/validator.js
@@ -0,0 +1,18 @@
+const {validationResult} = require('express-validator');
+
+/**
+ * For validating the inputs
+ * @param {Array<ValidationChain>} validations
+ */
+export const inputs = validations => {
+  return async (req, res, next) => {
+    await Promise.all(validations.map(validation => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    res.status(400).json({success: false, errors: errors.array()});
+  };
+};
