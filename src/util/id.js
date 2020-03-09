68c6ede9f419bd4172409a5d8d2ce15de941910d
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-03-09 20:44:53 +0100

    Switched id to module.exports

--- src/util/id.js
+++ src/util/id.js
@@ -1,20 +1,22 @@
-const ID = require('../models/id')
-const logger = require ('./logger')
+const ID = require('../models/id');
+const logger = require('./logger');
+
+const idUtil = {};
 
 /**
  *  Generates and returns id string
  * @param {number} length of id to make
  * @returns {string} id
  */
-export const makeId = (length) => {
-    const char = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-    let str = '';
+idUtil.makeId = length => {
+  const char = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+  let str = '';
 
-    for (let i = 1; i <= length; i++) {
-        str += char.charAt(Math.floor(Math.random() * char.length));
-    }
+  for (let i = 1; i <= length; i++) {
+    str += char.charAt(Math.floor(Math.random() * char.length));
+  }
 
-    return str;
+  return str;
 };
 
 /**
@@ -22,37 +24,39 @@ export const makeId = (length) => {
  * An async function that runs in parallel as a job
  * generates 500 id
  */
-export const generateIds = async () => {
-    // Generate 500 Id's
-    for (let i = 1; i <= 500; i++) {
-        const id = makeId(9);
-        // Check if the id exist
-        const idExist = await ID.exists({id});
-        if (idExist) {
-            i--; // Repeat the current iteration
-            continue;
-        }
-        // Id does not exist save id
-        await ID.create({id, used: false});
+idUtil.generateIds = async () => {
+  // Generate 500 Id's
+  for (let i = 1; i <= 500; i++) {
+    const id = idUtil.makeId(9);
+    // Check if the id exist
+    const idExist = await ID.exists({id});
+    if (idExist) {
+      i--; // Repeat the current iteration
+      continue;
     }
+    // Id does not exist save id
+    await ID.create({id, used: false});
+  }
 };
 
 /*
     Returns a generated id, if an id is not available
     it runs the generateIds job to generate an id's
  */
-export const getId = async () => {
-    let idDoc = await ID.findOneAndUpdate({used: false}, {used: true});
-    if (idDoc === null) {
-        // Run this parallel don't await this function
-        generateIds().catch(err => logger.error(err));
-    }
-    // Keep on checking for a new idDoc
-    // Till a new one is available
-    while (idDoc == null) {
-        idDoc = (await ID.findOne({used: false})) as IdModelType;
-    }
+idUtil.getId = async () => {
+  let idDoc = await ID.findOneAndUpdate({used: false}, {used: true});
+  if (idDoc === null) {
+    // Run this parallel don't await this function
+    idUtil.generateIds().catch(err => logger.error(err));
+  }
+  // Keep on checking for a new idDoc
+  // Till a new one is available
+  while (idDoc == null) {
+    idDoc = await ID.findOne({used: false});
+  }
 
-    // id as been found
-    return idDoc.id as string;
+  // id as been found
+  return idDoc.id;
 };
+
+module.exports = idUtil;
